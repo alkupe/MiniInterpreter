98a654fc589f800a3934a4a9106f3f8fb44e8083
alkupe@hotmail.com <alkupe@hotmail.com>
2014-04-07 22:52:52 -0400

    adding

---
+++ interpreter.ml
@@ -0,0 +1,14 @@
+(* File interpretor.ml *)
+open Parsing;;
+try
+  let lexbuf = Lexing.from_channel stdin in
+  while true do
+    try
+      MiniYACC.prog MiniLEX.token lexbuf
+    with Parse_error ->
+      (print_string "Syntax error ..." ; print_newline ()) ;
+    clear_parser ()
+  done
+with MiniLEX.Eof ->
+  ()
+;;
